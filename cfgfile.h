d14c6e776ea273f05f3d5c26ece506097b226af0
John Tsiombikas <nuclear@member.fsf.org>
2008-04-09 08:41:05 +0000

    Oh well, I guess there is no other way than duplicating the cfgfile files...
    git-svn-id: svn+ssh://svn.code.sf.net/p/spacenav/code/trunk/spnavcfg@43 ef983eb1-d774-4af8-acfd-baaf7b16a646

---
+++ cfgfile.h
@@ -0,0 +1,31 @@
+/*
+spacenavd - a free software replacement driver for 6dof space-mice.
+Copyright (C) 2007 John Tsiombikas <nuclear@siggraph.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CFGFILE_H_
+#define CFGFILE_H_
+
+struct cfg {
+	float sensitivity;
+	int dead_threshold;
+	int invert[6];
+};
+
+void default_cfg(struct cfg *cfg);
+int read_cfg(const char *fname, struct cfg *cfg);
+int write_cfg(const char *fname, struct cfg *cfg);
+
+#endif	/* CFGFILE_H_ */
